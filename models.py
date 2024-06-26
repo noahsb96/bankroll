fc192d6802d2507104a56ed3b7f43cd3aebedc28
noahsb96 <noahsb96@yahoo.com>
2024-06-25 23:10:45 -0600

    Adding user to models as I think it will be needed for each user to add singles

--- models.py
+++ models.py
@@ -1,6 +1,16 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DateTime, Boolean
 from sqlalchemy.orm import relationship
 from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    singles = relationship("Single", back_populates="owner")
 
 class Month(Base):
     __tablename__ = "months"
@@ -27,3 +37,6 @@ class Single(Base):
     profit = Column(String)
     month_bankroll_id = Column(Integer, ForeignKey('months.month_year'))
     month_bankroll = relationship("Month", back_populates="singles")
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="singles")
