3e497c61f1ed13acf830d198454eee4249c3499c
noahsb96 <noahsb96@yahoo.com>
2024-07-17 00:02:55 -0600

    Created post route. Going to create simple get route to test. I assume I will get errors but I want to see what errors I get back so I can debug issues.

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ from .database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/singles/", response_model=schemas.Single)
+def create_single(single: schemas.SingleCreate, db: Session = Depends(get_db)):
+    return crud.create_single(db=db, single=single)
