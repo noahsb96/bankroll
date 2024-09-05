65ba15408745a28b5a7898fead2e5ad572d570c3
noahsb96 <noahsb96@yahoo.com>
2024-09-04 23:44:52 -0600

    Trying to figure out how to post single to database.

--- crud.py
+++ crud.py
@@ -1,23 +1,27 @@
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy import Date, cast
 from datetime import datetime, date
 from typing import Any
 import models, schemas
 
-def profit_calculation(single: Any) -> None:
-    unit_size = single.month.unit_size
+def profit_calculation(single: schemas.SingleCreate) -> dict:
+    unit_size = single.unit_size
     wager = unit_size * single.units
+    profit_number = 0
     if single.result == "won":
-        single.wager = wager
-        single.profit_number = single.odds * wager
+        profit_number = single.odds * wager
     elif single.result == "lost":
-        single.wager = wager
-        single.profit_number = -wager
+        profit_number = -wager
     elif single.result == "push":
-        single.wager = wager
-        single.profit_number = 0
+        profit_number = 0
     else:
         raise ValueError("Incorrect Result Entered. Enter Win, Loss, Or Push")
+    
+    return {
+        'wager': wager,
+        'profit_number': profit_number
+    }
 
 def get_daily_bets(db: Session, date: datetime = datetime.now().date()):
     return db.query(models.Single).filter(cast(models.Single.timestamp, Date) == date).all()
@@ -37,15 +41,38 @@ def get_bankrolls(db: Session, limit: int = 100):
 def get_bankrolls_by_month(db: Session, month: date):
     return db.query(models.Month).filter(cast(models.Single.month_bankroll, Date) == month)
 
-def create_single(db: Session, single: schemas.SingleCreate, month: schemas.Month):
+def create_single(db: Session, single: schemas.SingleCreate):
     single_timestamp = datetime.now() if single_timestamp is None else single_timestamp
     single_profit = profit_calculation(single)
+    wager = single_profit['wager']
+    profit_number = single_profit['profit_number']
     month_key = single.month_bankroll.strftime('%m/%Y')
-    month_exists = db.query(models.Month).filter_by(month_year=month_key).first() is not None
-    db_month = models.Month(month_year=month_key, bankroll=month.bankroll, unit_size=month.unit_size)
-    db_single=models.Single(result=single.result, pick=single.pick, sport=single.sport, units=single.units, odds=single.odds, timestamp=single_timestamp, profit=single_profit, wager=single.wager, month_bankroll=month_key, net_profit=single_profit-single.wager)
-    if month_exists:
-        pass
-    else:
+    db_month = db.query(models.Month).filter_by(month_year=single.month_bankroll).first()
+    if db_month is None:
+        db_month = models.Month(
+            month_year=single.month_bankroll,
+            bankroll=single.bankroll,
+            unit_size=single.unit_size
+            )
         db.add(db_month)
+    db_single=models.Single(
+        result=single.result,
+        pick=single.pick,
+        sport=single.sport,
+        units=single.units,
+        odds=single.odds,
+        timestamp=single_timestamp,
+        profit=profit_number,
+        net_profit=profit_number - wager,
+        wager=wager,
+        month_bankroll=single.month_bankroll
+        )
+    db_single.month = db_month
     db.add(db_single)
+    try:
+        db.commit()
+        db.refresh(db_single)
+        return db_single
+    except IntegrityError:
+        db.rollback()
+        raise
